592526ff5b3c1a91a578efe9ed5fcc8832176d8d
HernandezIsaac <145407452+HernandezIsaac@users.noreply.github.com>
2023-10-08 21:21:46 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// Variables to store my first name and last name
+var firstName = "Issac";
+var lastName = "Hernandez";
+
+var sentence = "Armored Core 6 - Fires of Rubicon";
+var theTruth = true;
+
+// variables to store some numbers
+var numberOne = 10;
+var numberTwo = 15;
+var numberThree = 25;
+
+
+// Below line prints my full name
+console.log(firstName + " " + lastName);
+
+// Below line finds the length of 'sentence' variable's content and prints it
+console.log("The length of my sentence is: " + sentence.length + ".");
+
+console.log("The sum of " + numberOne + " and " + numberTwo + " is equal to " + numberThree + ".");
+
+console.log("The variable theTruth is storing the value " + theTruth + "." + " The data type of theTruth is " + typeof theTruth + 
+".")
+
+
